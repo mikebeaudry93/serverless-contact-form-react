05ae0f3947b41c21f07edb0dae1f16543ce9ac2f
Mike Beaudry <michaelbeaudry93@gmail.com>
2020-08-27 20:01:09 -0400

    final

--- src/Contact.js
+++ src/Contact.js
@@ -28,7 +28,7 @@ class Contact extends Component {
     let data = this.state.formData;
 
     axios
-      .post("API_URI", data)
+      .post("https://serverless-contact-from-axios-ejvr0xudi.vercel.app/", data)
       .then((res) => {
         this.setState({ sent: true }, this.resetForm());
       })
